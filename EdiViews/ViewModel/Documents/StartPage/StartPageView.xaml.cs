974da0e83197992cd7b9c144acfa99cfbe8fcf08
dirkster99 <dirk.bahle@gmx.de>
2014-07-28 18:48:40 +0200

    Stabalizing Explorer TW

---
+++ EdiViews/ViewModel/Documents/StartPage/StartPageView.xaml.cs
@@ -0,0 +1,15 @@
+namespace EdiViews.ViewModel.Documents.StartPage
+{
+  using System.Windows.Controls;
+
+  /// <summary>
+  /// Interaction logic for StartPageView.xaml
+  /// </summary>
+  public partial class StartPageView : UserControl
+  {
+    public StartPageView()
+    {
+      this.InitializeComponent();
+    }
+  }
+}
