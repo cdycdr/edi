ca8ef578f575819bbb45cdfb074ed11dd838394a
dirkster99 <dirk.bahle@gmx.de>
2014-10-15 21:56:36 +0200

    Moved  IToolWindow file

---
+++ Edi.Core/Interfaces/IToolWindow.cs
@@ -0,0 +1,21 @@
+﻿namespace Edi.Core.ViewModels
+{
+	using Edi.Core.Interfaces.Enums;
+
+	public interface IToolWindow
+	{
+		#region properties
+		string Name	{ get; }
+
+		bool IsVisible { get; }
+
+		PaneLocation PreferredLocation { get; }
+		double PreferredWidth { get; }
+		double PreferredHeight { get; }
+		#endregion properties
+
+		#region methods
+		void SetToolWindowVisibility(bool isVisible = true);
+		#endregion methods
+	}
+}
