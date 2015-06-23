2d3c7d7f0f5ca6beb37a83f205bf9ba81c35995f
Dirkster99 <dirk.bahle@gmx.de>
2015-06-23 19:40:30 +0200

    updated version info in avalonedit

--- ICSharpCode.AvalonEdit/Highlighting/HighlightingManager.cs
+++ ICSharpCode.AvalonEdit/Highlighting/HighlightingManager.cs
@@ -1,19 +1,32 @@
-﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
-// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+﻿// Copyright (c) 2014 AlphaSierraPapa for the SharpDevelop Team
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+using ICSharpCode.AvalonEdit.Utils;
+using ICSharpCode.AvalonEdit.Highlighting.Themes;
 
 namespace ICSharpCode.AvalonEdit.Highlighting
 {
-  using System;
-  using System.Collections.Generic;
-  using System.Collections.ObjectModel;
-  using System.Diagnostics;
-  using System.IO;
-  using System.Xml;
-
-  using ICSharpCode.AvalonEdit.Highlighting.Themes;
-  using ICSharpCode.AvalonEdit.Utils;
-  using System.Windows.Media;
-
   /// <summary>
 	/// Manages a list of syntax highlighting definitions.
 	/// </summary>
@@ -137,7 +150,7 @@ namespace ICSharpCode.AvalonEdit.Highlighting
     }
 
     /// <summary>
-    /// Get/set property to undo customization changes applied to <seealso cref="SolidColorBrush"/>
+    /// Get/set property to undo customization changes applied to SolidColorBrush
     /// dynamic resources (these resources need to be reset to null on next chnage of theme or they
     /// will not be set correctly when the user switches themes twice).
     /// 
